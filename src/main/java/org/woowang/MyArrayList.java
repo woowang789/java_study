99930a15c8b7b244cfef59bf965d9bf2ba6336c5
woowang789 <wangjh789@gmail.com>
2022-07-15 10:39:30 +0900

    first commit & solve ArrayList_클래스가_존재한다

---
+++ src/main/java/org/woowang/MyArrayList.java
@@ -0,0 +1,7 @@
+package org.woowang;
+
+public class MyArrayList {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/org/woowang/MyLinkedListTest.java
@@ -0,0 +1,129 @@
+package org.woowang;
+
+
+import org.junit.jupiter.api.Test;
+
+class MyLinkedListTest {
+    @Test
+    void ArrayList_클래스가_존재한다() {
+        MyArrayList al = new MyArrayList();
+    }
+
+//    @Test
+//    void size() {
+//        ArrayList al = new ArrayList();
+//        assertEquals(0, al.size());
+//    }
+//
+//    @Test
+//    void add() {
+//        ArrayList al = new ArrayList();
+//        al.add(100);
+//    }
+//
+//    @Test
+//    void get() {
+//        ArrayList al = new ArrayList();
+//        al.add(100);
+//        int rs = al.get(0);
+//
+//        assertEquals(100, rs);
+//    }
+//
+//    @Test
+//    void add를_한번_할_때_마다_size가_증가() {
+//        ArrayList al = new ArrayList();
+//        al.add(100);
+//        al.add(200);
+//        al.add(300);
+//
+//        assertEquals(3, al.size());
+//    }
+//
+//    @Test
+//    void get_1_equals_200() {
+//        ArrayList al = new ArrayList();
+//        al.add(100);
+//        al.add(200);
+//        al.add(300);
+//        int rs = al.get(1);
+//
+//        assertEquals(200, rs);
+//    }
+//
+//    @Test
+//    void removeAt() {
+//        ArrayList al = new ArrayList();
+//        al.add(100);
+//        al.add(200);
+//        al.add(300);
+//
+//        al.removeAt(1);
+//    }
+//
+//    @Test
+//    void removeAt을_실행하면_size가_1_줄어든다() {
+//        ArrayList al = new ArrayList();
+//        al.add(100);
+//        al.add(200);
+//        al.add(300);
+//
+//        assertEquals(3, al.size());
+//
+//        al.removeAt(1);
+//
+//        assertEquals(2, al.size());
+//    }
+//
+//    @Test
+//    void removeAt을_실행하면_뒷좌석에_있는_데이터들이_전부_한칸씩_앞으로_이동() {
+//        ArrayList al = new ArrayList();
+//        al.add(100);
+//        al.add(200);
+//        al.add(300);
+//
+//        assertEquals(200, al.get(1));
+//
+//        al.removeAt(1);
+//
+//        assertEquals(300, al.get(1));
+//    }
+//
+//    @Test
+//    void 최초에_배열의_크기는_2_이다() {
+//        ArrayList al = new ArrayList();
+//
+//        assertEquals(2, al.getArrayLength());
+//    }
+//
+//    @Test
+//    void 배열이_꽉_차면_2배_크기로_증가해야_한다() {
+//        ArrayList al = new ArrayList();
+//        al.add(100);
+//        al.add(200);
+//        assertEquals(2, al.getArrayLength());
+//        al.add(300);
+//        assertEquals(4, al.getArrayLength());
+//    }
+//
+//    @Test
+//    void showAllValues() {
+//        ArrayList al = new ArrayList();
+//        al.add(1000);
+//        al.add(200);
+//        al.add(30);
+//
+//        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+//
+//        al.showAllValues();
+//
+//        String rs = output.toString();
+//
+//        TestUtil.clearSetOutToByteArray(output);
+//
+//        assertTrue(rs.contains("== 전체 데이터 출력 =="));
+//        assertTrue(rs.contains("0 : 1000"));
+//        assertTrue(rs.contains("1 : 200"));
+//        assertTrue(rs.contains("2 : 30"));
+//    }
+}
